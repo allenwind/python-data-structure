8d4a7842790e56a8cfec07e03abf3d4b02377a3b
allenwind <allenwind@foxmail.com>
2020-06-16 04:24:43 +0800

    bot auto push

---
+++ cpp/list/doubly_linked_list.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+struct ListNode
+{
+	int value;
+	ListNode* prev;
+	ListNode* next;
+};
+
+class DoublyLinkedList
+{
+public:
+	DoublyLinkedList()
+	{
+		
+	}
+}
